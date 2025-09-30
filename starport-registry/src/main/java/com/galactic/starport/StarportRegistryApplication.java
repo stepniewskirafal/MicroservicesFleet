f7db05d50e5538b6ebf0c8040091eff9287a8dd1
Rafal Stepniewski <vultur12@gmail.com>
2025-09-30 07:14:24 +0200

    eureka-server

---
+++ starport-registry/src/main/java/com/galactic/starport/StarportRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.galactic.starport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarportRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarportRegistryApplication.class, args);
+	}
+
+}
---
+++ starport-registry/src/test/java/com/galactic/starport/StarportRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galactic.starport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarportRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
