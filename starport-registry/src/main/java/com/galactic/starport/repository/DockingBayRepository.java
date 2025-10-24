e896384a373521a7f16e45dd88d91ab15d8f481c
Rafal Stepniewski <vultur12@gmail.com>
2025-10-25 01:40:01 +0200

    starport-registry: refactor

--- starport-registry/src/main/java/com/galactic/starport/repository/DockingBayRepository.java
+++ starport-registry/src/main/java/com/galactic/starport/repository/DockingBayRepository.java
@@ -8,19 +8,25 @@ import org.springframework.data.repository.query.Param;
 
 public interface DockingBayRepository extends JpaRepository<DockingBayEntity, Long> {
     @Query(
-            value = "select docking_bay.* "
-                    + " from starport"
-                    + " join docking_bay"
-                    + "    on starport.id = docking_bay.starport_id"
-                    + " where starport.code = :starportCode"
-                    + " and docking_bay.status = 'AVAILABLE'"
-                    + " and docking_bay.ship_class = :shipClass"
-                    + "  AND NOT EXISTS ("
-                    + "    SELECT 1"
-                    + "    FROM reservation r"
-                    + "    WHERE docking_bay.id = r.docking_bay_id"
-                    + "      AND r.start_at < :endAt "
-                    + "      AND r.end_at   > :startAt )",
+            value =
+                    """
+                    select docking_bay.*
+                    from starport
+                    join docking_bay
+                       on starport.id = docking_bay.starport_id
+                    where starport.code = :starportCode
+                    and docking_bay.status = 'AVAILABLE'
+                    and docking_bay.ship_class = :shipClass
+                     AND NOT EXISTS (
+                       SELECT 1
+                       FROM reservation r
+                       WHERE docking_bay.id = r.docking_bay_id
+                         AND r.status IN ('HOLD', 'CONFIRMED')
+                         OR ( r.start_at < :endAt
+                         AND r.end_at   > :startAt ))
+                    FOR UPDATE SKIP LOCKED
+                    LIMIT 1
+                    """,
             nativeQuery = true)
     Optional<DockingBayEntity> findFreeBay(
             @Param("starportCode") String starportCode,
